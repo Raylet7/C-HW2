8c9328a581a681d04d0c4016c4f65e78b1c88bc1
Svetlana <tsvetcova.s@mail.ru>
2022-11-06 22:36:34 +0300

    add Project 1 Get Second Digit

---
+++ Project1_GetSecondDigit/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, принимающая трёхзначное число и показывающая вторую цифру
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+int GetSecondDigit(int number)
+{
+    int secondDigit = number / 10 % 10;
+    return secondDigit;
+}
+Console.WriteLine("Введите трёхзначное число");
+int number = int.Parse(Console.ReadLine()!);
+int secondDigit = GetSecondDigit(number);
+Console.WriteLine("Вторая цифра числа: " + secondDigit);
