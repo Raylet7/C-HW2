a7bac5e0059d724d3f38eb70a5bca844995ff3d3
Svetlana <tsvetcova.s@mail.ru>
2022-11-07 15:09:34 +0300

    add Project 3 Is Holiday

---
+++ Project3_IsHoliday/Program.cs
@@ -0,0 +1,36 @@
+﻿// Программа, принимающая цифру, обозначающую день недели, 
+//и проверяющая выходной ли этот день
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+bool IsWeekDay(int WeekDayNumber)
+{
+    return WeekDayNumber > 0 && WeekDayNumber < 8;
+}
+bool IsHoliday(int WeekDayNumber)
+{
+    return WeekDayNumber == 6 || WeekDayNumber == 7;
+}
+Console.WriteLine("Введите цифру от 1 до 7, обозначающую день недели ");
+int WeekDayNumber;
+while (!int.TryParse(Console.ReadLine()!, out WeekDayNumber))
+{
+    Console.WriteLine("Неверный ввод");
+    Console.WriteLine("Введите цифру, обозначающую день недели ");
+}
+if (IsWeekDay(WeekDayNumber))
+{
+    if (IsHoliday(WeekDayNumber))
+    {
+        Console.WriteLine("Да, день выходной");
+    }
+    else
+    {
+        Console.WriteLine("Нет, день не выходной");
+    }
+}
+else
+{
+    Console.WriteLine("Неверный ввод");
+    Console.WriteLine("Попробуйте снова запустить программу");
+}
